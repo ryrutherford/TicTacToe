043c6431606d5c433b0d1a89d065478dfaf6cf99
Ry Rutherford <ry.rutherford99@gmail.com>
2019-10-24 23:58:18 -0400

    Created TicTacToe class and added .gitignore to bin folder

---
+++ TicTacToe/src/TicTacToe.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class TicTacToe {
+	/*the board is represented by an ArrayList
+	 * --> -1 means there is an empty slot
+	 * --> 0 means there is an O in the slot
+	 * --> 1 means there is an X in the slot
+	 */
+	private ArrayList<Integer> board;
+}
