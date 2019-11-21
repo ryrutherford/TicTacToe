0279a029166b9d979ef1fcd6dac685a1f7e1b51b
Ry Rutherford <ry.rutherford99@gmail.com>
2019-11-21 01:04:40 -0500

    Changed the data structure representing the board (ArrayList -> 2D Array). Changed the gameWon() method -> improved methodology for checking if a game was won (eliminated need for three attributes monitoring status of rows, cols, diags). Added a new attribute that lets the player specify how many consecutive X's or 0's are needed to win the game.

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		TicTacToe t = new TicTacToe(3,2);
+		TicTacToe t = new TicTacToe(5,2,5);
 		t.play();
 	}
 }
--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class TicTacToe {
 	/* ATTRIBUTES
-	 * board: the board is represented by an ArrayList
+	 * map: the board is represented by a 2d matrix
 	 * --> -1 means there is an empty slot
 	 * --> 0 means there is an O in the slot
 	 * --> 1 means there is an X in the slot
@@ -15,22 +15,11 @@ public class TicTacToe {
 	 * 
 	 * size: the size of the board is represented by a positive integer (width*width)
 	 * 
+	 * toWin: an integer that represents the number of consecutive X's or 0's that are needed to win
+	 * 
 	 * numMoves: the number of moves made is represented by a positive inteer
 	 * 
 	 * move: the move inputted by the user is retrieved by the Scanner object
-	 * 
-	 * BOARD TRACKERS
-	 * row: a width X 2 matrix that stores the status of the ith row in row[i][0] and the capacity of the ith row in row[i][1]
-	 * --> the status can be either:
-	 * 		-2: the row is dead and cannot be won
-	 * 		-1: the row has no inputs
-	 * 		0: the row can be won by player A
-	 * 		1: the row can be won by player B
-	 * --> the capacity will range from 0 to width (if the capacity is 0 the row is empty)
-	 * 		a row can be won <--> the capacity is equal to width
-	 * col: a width X 2 matrix that stores the status of the ith column in col[i][0] and the capacity of the ith column in col[i][1]
-	 * diag: a 2 X 2 matrix that stores the status and capacity of the south east diagonal in diag[0][0] and diag[0][1] respectively
-	 * 		 the status and capacity of the south west diagonal are stored in diag[1][0] and diag[1][1] respectively
 	 * 
 	 * numPlayers: the number of players in the game are stored in an integer
 	 * --> this attribute isn't necessary right now but it will be if the game is expanded to include more players
@@ -38,15 +27,14 @@ public class TicTacToe {
 	 * names: the names of the players are stored in an integer array called names
 	 */
 	
-	private ArrayList<Integer> board;
+	//private ArrayList<Integer> board;
+	private int[][] map;
 	private int turn;
 	private int width;
 	private int size;
+	private int toWin;
 	private int numMoves;
 	private Scanner move;
-	private int[][] row;
-	private int[][] col;
-	private int[][] diag = {{-1,0},{-1,0}};
 	private int numPlayers;
 	private int[] names;
 	
@@ -55,7 +43,7 @@ public class TicTacToe {
 	 * it also initializes an empty board with -1's
 	 */
 	
-	public TicTacToe(int width, int numPlayers) {
+	public TicTacToe(int width, int numPlayers, int toWin) {
 		//minimum board size is 3x3
 		if(width < 3) {
 			width = 3;
@@ -64,12 +52,17 @@ public class TicTacToe {
 		//initializing the scanner
 		this.move = new Scanner(System.in);
 		
-		//initializing the board
+		//initializing the toWin
+		this.toWin = toWin;
 		this.size = width*width;
 		this.width = width;
-		this.board = new ArrayList<Integer>(this.size);
-		for(int i = 0; i < this.size; i++) {
-			this.board.add(i,-1);
+		
+		//initializing the map
+		this.map = new int[width][width];
+		for(int i = 0; i < this.width; i++) {
+			for(int j = 0; j < this.width; j++) {
+				this.map[i][j] = -1;
+			}
 		}
 		
 		//randomly assigns the first turn to player A or player B
@@ -81,40 +74,94 @@ public class TicTacToe {
 		for(int i = 0; i < this.numPlayers; i++) {
 			names[i] = i;
 		}
-		
-		//initializing the board trackers
-		this.row = new int[this.width][2];
-		this.col = new int[this.width][2];
-		for(int i = 0; i < this.width; i++) {
-			this.row[i][0] = -1;
-			this.row[i][1] = 0;
-			this.col[i][0] = -1;
-			this.col[i][1] = 0;
-		}
 	}
 	
 	/*
 	 * GAME WON METHOD
 	 * if a player has won, return true, else return false
-	 * three variables track the status of a row, and the columns and diagonals
-	 * if any one of these variables stores the value 0 or this.width, then a player has won
 	 */
 	
 	private boolean gameWon() {
 
-		//checking whether a Player has won on a column, diagonal, or a row
-		for(int k = 0; k < this.width; k++) {
-			for(int i = 0; i < 2; i++) {
-				//if the col, diag, and row is equal to i (0,1) and the col or row is full --> a player has won
-				boolean colWin = (col[k][0] == i && col[k][1] == this.width);
-				boolean rowWin = row[k][0] == i && row[k][1] == this.width;
-				boolean diagWin = diag[i][0] == 0 && diag[i][1] == this.width;
-				diagWin = diagWin || diag[i][0] == 1 && diag[i][1] == this.width;
-				
-				if(colWin || rowWin || diagWin) {
-					System.out.println("Player " + this.names[(this.turn + 1)%2] + " wins!");
-					return true;
+		
+		int prev; //prev stores a predecessor value for the matrix value at row i, col j
+		int k; //k will be used to iterate through the predecessors in a row, col, or diag
+		
+		//the nested for loop iterates through each map entry and uses the indices to check for 'toWin' consecutive X's or O's
+		for(int i = 0; i < this.width; i++) {
+			for(int j = 0; j < this.width; j++) {
+
+				//ROW CHECK
+				int cur = this.map[i][j];
+				try {
+					for(k = 1; k < this.toWin; k++) {
+						prev = this.map[i][j-k];
+						if(prev != cur || cur == -1) { //if prev and cur don't match or cur = -1 then the row hasn't been won
+							break;
+						}
+					}
+					if(k == this.toWin) { //if we iterated through the whole loop then a player has won
+						System.out.println("Player " + cur + " has won! Congratulations.");
+						return true;
+					}
 				}
+				catch(Exception e) {
+					//do nothing, array index out of bounds or negative index exception was thrown
+				}
+				
+				//SW DIAG CHECK
+				try {
+					for(k = 1; k < this.toWin; k++) {
+						prev = this.map[i-k][j-k];
+						if(prev != cur || cur == -1) { //if prev isn't equal to cur or cur = -1 then the SW diag hasn't been won
+							break;
+						}
+					}
+					if(k == this.toWin) { //if we iterated through the whole loop then a player has won
+						System.out.println("Player " + cur + " has won! Congratulations.");
+						return true;
+					}
+				}
+				catch(Exception e) {
+					//do nothing, array index out of bounds or negative index exception was thrown
+				}
+				
+				//SE DIAG CHECK
+				try {
+					for(k = 1; k < this.toWin; k++) {
+						prev = this.map[i-k][j+k];
+						if(prev != cur || cur == -1) { //if prev isn't equal to cur or cur = -1 then the SE diag hasn't been won
+							break;
+						}
+					}
+					if(k == this.toWin) { //if we iterated through the whole loop then a player has won
+						System.out.println("Player " + cur + " has won! Congratulations.");
+						return true;
+					}
+				}
+				catch(Exception e) {
+					//do nothing, array index out of bounds or negative index exception was thrown
+				}
+				
+				//COL CHECK
+				cur = this.map[j][i];
+				try {
+					for(k = 1; k < this.toWin; k++) {
+						prev = this.map[i-k][j];
+						if(prev != cur || cur == -1) { //if prev isn't equal to cur or cur = -1 then the SW diag hasn't been won
+							break;
+						}
+					}
+					if(k == this.toWin) { //if we iterated through the whole loop then a player has won
+						System.out.println("Player " + cur + " has won! Congratulations.");
+						return true;
+					}
+				}
+				catch(Exception e) {
+					//do nothing, array index out of bounds or negative index exception was thrown
+				}
+				
+				
 			}
 		}
 		return false;
@@ -122,68 +169,25 @@ public class TicTacToe {
 	
 	/*
 	 * MOVE MADE METHOD
-	 * location[0] is the column, location[1] is the row
+	 * location[0] is the row, location[1] is the column
 	 */
 	
 	private void moveMade(int[] location) {
-		//calculates the index in the board based on the column, row, and width
 		try {
 			System.out.println("Attempting to execute player " + this.turn + "'s move");
-			int index = location[0] + this.width*location[1];
-			if(this.board.get(index) == -1) {
+			int row = location[0];
+			int col = location[1];
+			if(this.map[row][col] == -1) {
 				
 				//increasing the number of moves
 				this.numMoves++;
 				
 				//making the move
-				this.board.set(index, this.turn);
+				//this.board.set(index, this.turn);
+				this.map[row][col] = this.turn;
 				System.out.println("Move Successful.");
 				
-				//updating the value stored in the row capacity tracker
-				this.row[location[1]][1] += 1;
-				//updating the value stored in the row status tracker --> a value of -2 means the row is dead
-				if(this.row[location[1]][0] == turn || this.row[location[1]][0] == -1) {
-					this.row[location[1]][0] = turn;
-				}
-				else {
-					this.row[location[1]][0] = -2;
-				}
-				
-				//updating the value stored in the col capacity tracker
-				this.col[location[0]][1] += 1;
-				//updating the value stored in col tracker --> a value of -2 means the col is dead
-				if(this.col[location[0]][0] == turn || this.col[location[0]][0] == -1) {
-					this.col[location[0]][0] = turn;
-				}
-				else {
-					this.col[location[0]][0] = -2;
-				}
-				
-				//updating the value stored in diag tracker --> a value of -2 means the diag is dead
-				//checking the South East diag
-				if(index%(width+1) == 0) {
-					//updating the value stored in the diag SE capacity tracker
-					this.diag[0][1] += 1;
-					if(this.diag[0][0] == turn || this.diag[0][0] == -1) {
-						this.diag[0][0] = turn;
-					}
-					else {
-						this.diag[0][0] = -2;
-					}
-				}
-				//checking the South West diag
-				if((index%(width-1) == 0) && (index != 0) && (index != size-1)) {
-					//updating the value stored in the diag SW capacity tracker
-					this.diag[1][1] += 1;
-					if(this.diag[1][0] == turn || this.diag[1][0] == -1) {
-						this.diag[1][0] = turn;
-					}
-					else {
-						this.diag[1][0] = -2;
-					}
-				}
-
-				//if the turn is 1 then it will become 0, if the turn is 0 then it will become 1
+				//updating the turn
 				this.turn = (this.turn + 1)%2;
 			}
 			else {
@@ -205,21 +209,12 @@ public class TicTacToe {
 	
 	private void clearBoard() {
 		//resetting the board
-		for(int i = 0; i < this.size; i++) {
-			this.board.set(i, -1);
-		}
-		
-		//resetting the board trackers
 		for(int i = 0; i < this.width; i++) {
-			this.row[i][0] = -1;
-			this.row[i][1] = 0;
-			this.col[i][0] = -1;
-			this.col[i][1] = 0;
+			//this.board.set(i, -1);
+			for(int j = 0; j < this.width; j++) {
+				this.map[i][j] = -1;
+			}
 		}
-		this.diag[0][0] = -1;
-		this.diag[0][1] = 0;
-		this.diag[1][0] = -1;
-		this.diag[1][1] = 0;
 		
 		//randomizing the turn again
 		this.turn = (int)(2*Math.random());
@@ -232,13 +227,14 @@ public class TicTacToe {
 	 * PLAY METHOD
 	 * invoke this method to play a game of Tic Tac Toe 
 	 */
+	
 	public void play() {
 		System.out.println("Welcome to TicTacToe. Please note that the minimum board size is 3X3. Here is the board:");
 		System.out.println(this);
 		while(this.numMoves < this.size) {
 			try {
-				System.out.println("Player " + this.names[turn] + "'s turn. Enter a move as 'Column #' press Enter"
-						+ " then 'Row #' and press Enter again i.e. 0 'press enter' 0 'press enter'");
+				System.out.println("Player " + this.names[turn] + "'s turn. Enter a move as 'Row #' press Enter"
+						+ " then 'Column #' and press Enter again i.e. 0 'press enter' 0 'press enter'");
 				//parsing the user's input
 				int[] location = new int[2];
 				location[0] = this.move.nextInt();
@@ -249,6 +245,9 @@ public class TicTacToe {
 				
 				//if the game has been won --> the players can play again if they want
 				if(this.gameWon() == true || this.numMoves == size) {
+					if(this.numMoves == size) {
+						System.out.println("It's a tie!");
+					}
 					System.out.println("Do you want to play again? Enter yes or no.");
 					
 					//garbage collector
@@ -260,6 +259,7 @@ public class TicTacToe {
 					if(answer.equals("yes")) {
 						System.out.println("Starting a new game.");
 						this.clearBoard();
+						System.out.println("Here is the board: " + '\n' + this);
 					}
 					else if(answer.equals("no")) {
 						System.out.println("Thanks for playing!");
@@ -271,8 +271,10 @@ public class TicTacToe {
 					}
 				}
 			}
-			catch(InputMismatchException e1) {
+			catch(InputMismatchException e) {
 				System.out.println("You did not enter an integer number. Try again.");
+				//garbage collector
+				this.move.nextLine();
 			}
 			catch(Exception e) {
 				System.out.println("Something went wrong. Try again.");
@@ -284,6 +286,7 @@ public class TicTacToe {
 	/*
 	 * DRAW BOARD METHOD
 	 */
+	
 	public String toString() {
 		String out = "";
 		
@@ -312,14 +315,12 @@ public class TicTacToe {
 			out+= "\n";
 			
 			//prints out the X's, O's, or empty boxes
-			int leftEdge = this.width*i;
-			int rightEdge = this.width*i + this.width;
-			for(int j = leftEdge; j < rightEdge; j++) {
-				if(j < rightEdge -1) {
-					if(this.board.get(j) == -1) {
+			for(int j = 0; j < this.width; j++) {
+				if(j < this.width -1) {
+					if(this.map[i][j]==-1) {
 						out += "|     ";
 					}
-					else if(this.board.get(j) == 0) {
+					else if(this.map[i][j]==0) {
 						out += "|  O  ";
 					}
 					else {
@@ -327,14 +328,14 @@ public class TicTacToe {
 					}
 				}
 				else {
-					if(this.board.get(j) == -1) {
-						out += "|     |   " + (j/this.width);
+					if(this.map[i][j] == -1) {
+						out += "|     |   " + (i);
 					}
-					else if(this.board.get(j) == 0) {
-						out += "|  O  |   " + (j/this.width);
+					else if(this.map[i][j] == 0) {
+						out += "|  O  |   " + (i);
 					}
 					else {
-						out += "|  X  |   " + (j/this.width);
+						out += "|  X  |   " + (i);
 					}
 				}
 
@@ -347,14 +348,15 @@ public class TicTacToe {
 	/*
 	 * GETTERS
 	 */
+	
 	public int getSize() {
 		return this.size;
 	}
 	public int getWidth() {
 		return this.width;
 	}
-	public ArrayList<Integer> getBoard() {
-		return this.board;
+	public int[][] getMap() {
+		return this.map;
 	}
 	public int getTurn() {
 		return this.turn;
